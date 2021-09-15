c467800747882f8f237888b93324d65e648fbc23
morralito <anthonyjosecortez@gmail.com>
2021-09-15 00:09:48 -0400

    agregadas animaciones y odernados el scoreboard

---
+++ script.js
@@ -0,0 +1,100 @@
+let playerScore = 0;
+let computerScore = 0;
+let player = document.getElementById('player');
+let computer = document.getElementById('computer');
+let roundResult = document.getElementById('roundResult');
+let finalResult = document.getElementById('finalResult');
+let jugarAgain = document.getElementById('playAgain');
+let congratulationScreen = document.getElementById('congratulations');
+let resetScore = () => computerScore = playerScore = 0;
+
+function computerPlay(){
+    const RPS = ["rock", "paper", "scissors"];
+    return RPS[Math.floor(Math.random() * (3 - 0) + 0)];
+}
+
+function roundWinner(playerSelection, computerSelection){
+   if(computerSelection === playerSelection){
+      return "It's a tie!";
+   } 
+   else {
+      switch(playerSelection){
+
+         case 'rock':
+            if (computerSelection === "paper"){
+               return `You lose! ${computerSelection} beats ${playerSelection} :(`;
+            }
+            else{
+               return `You win! ${playerSelection} beats ${computerSelection} :D`;
+            }
+
+         case 'paper':
+            if (computerSelection === "scissors"){
+               return `You lose! ${computerSelection} beats ${playerSelection} :(`;
+            }
+            else{
+               return `You win! ${playerSelection} beats ${computerSelection} :D`;
+            }
+
+         case 'scissors':
+         if (computerSelection === "rock"){
+               return `You lose! ${computerSelection} beats ${playerSelection} :(`
+            }
+            else{
+               return `You win! ${playerSelection} beats ${computerSelection} :D`
+            }
+          default: 
+            return "Invalid option, try again.";      
+      }
+   }
+}
+
+function playAgain() {
+  jugarAgain.style.display = "block";
+  lista.style.display = "none";
+  jugarAgain.addEventListener('click', e => {
+    if (e.returnValue){
+      resetScore();
+      player.innerText = `Player: ${playerScore}`;
+      computer.innerText = `Computer: ${computerScore}`;
+      jugarAgain.style.display = "none";
+      lista.style.display = "flex";
+    }
+  });
+}
+
+function game(pSelection){
+  
+   let result = roundWinner(pSelection,computerPlay());
+   roundResult.innerText = result;
+
+   console.log(result);
+   result = result.slice(0,8);
+   if ( result === "You win!"){
+       playerScore++; 
+   }
+   else if (result === "You lose"){
+       computerScore++;
+   }
+
+   player.innerText = `Player: ${playerScore}`;
+   computer.innerText = `Computer: ${computerScore}`;
+
+   if (playerScore === computerScore === 5 ){
+     playAgain();
+   }
+   else if (playerScore === 5) {
+    playAgain();
+   }
+   else if (computerScore === 5){
+    playAgain();
+   }
+}
+
+let lista = document.getElementById('container');
+lista.addEventListener('click', (e) => {
+    let choice = e.path[0].id;
+    console.log(choice);
+    if (!choice || choice === 'container') return;
+    game(choice);
+});
